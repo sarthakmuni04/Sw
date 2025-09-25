4d50e4c3f38a5f0bfe442032469131c536255db8
Sarthak Muni <sarthakmuni71@gmail.com>
2025-09-25 14:13:42 +0530

    connection with database established

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async(req, res) => {
+    res.send("Signup Route Called");
+};
+
+export const login = async(req, res) => {
+    res.send("Login Route Called");
+};
+
+export const logout = async(req, res) => {
+    res.send("Logout Route Called");
+};
